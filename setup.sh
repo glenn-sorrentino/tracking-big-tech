44c00d264f3f789b42e16259b49c3638258690ce
Glenn <hello@glennsorrentino.com>
2023-04-07 00:00:52 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 EOL
 
 # Write sample code to templates/index.html
